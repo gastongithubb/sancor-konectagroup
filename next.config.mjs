3b69bfa9c93d23f425dfc67a52216bef5d174c0e
Valentín Etchecopar <valentinetchecopar@MacBook-Pro-de-Gaston.local>
2024-09-02 06:29:38 -0300

    sss

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   // Aumentar el tiempo de espera para la generación de páginas estáticas (opcional)
   staticPageGenerationTimeout: 120,
+  experimental: {
+    serverActions: true,
+  }
 };
 
 export default nextConfig;
