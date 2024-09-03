2d91d9881adcb92a60488c34c7f21ea9a0a37f41
Gaston Alvarez <gastonalvarez604@gmail.com>
2024-09-02 22:58:22 -0300

    push

--- app/api/teams/route.ts
+++ app/api/teams/route.ts
@@ -1,11 +1,10 @@
-/* eslint-disable no-console */
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
 export async function GET() {
   try {
     const teams = await prisma.team.findMany({
-      select: { id: true, name: true },
+      select: { id: true, name: true, leaderId: true },
     });
     return NextResponse.json(teams);
   } catch (error) {
@@ -16,40 +15,22 @@ export async function GET() {
 
 export async function POST(request: NextRequest) {
   try {
-    const body = await request.json();
-    console.log('Received request body:', body);
-
-    const { name } = body;
+    const { name, leaderId } = await request.json();
 
     if (!name) {
-      console.log('Team name is missing');
       return NextResponse.json({ error: 'Team name is required' }, { status: 400 });
     }
 
-    console.log('Attempting to find default leader');
-    const defaultLeader = await prisma.user.findFirst({
-      where: { role: 'leader' },
-    });
-
-    if (!defaultLeader) {
-      console.log('No default leader found');
-      return NextResponse.json({ error: 'No default leader found' }, { status: 400 });
-    }
-
-    console.log('Creating new team');
     const newTeam = await prisma.team.create({
       data: {
         name,
-        leader: {
-          connect: { id: defaultLeader.id },
-        },
+        leaderId: leaderId || undefined, // Si no se proporciona leaderId, será undefined
       },
     });
 
-    console.log('New team created:', newTeam);
     return NextResponse.json(newTeam, { status: 201 });
   } catch (error) {
-    console.error('Error in POST /api/teams:', error);
-    return NextResponse.json({ error: 'Error creating team', details: error instanceof Error ? error.message : 'Unknown error' }, { status: 500 });
+    console.error('Error creating team:', error);
+    return NextResponse.json({ error: 'Error creating team' }, { status: 500 });
   }
 }
