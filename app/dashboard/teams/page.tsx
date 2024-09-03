fcb237f24cdc86b29cefaea7809c84ce876b7b14
Gaston Alvarez <gastonalvarez604@gmail.com>
2024-09-02 22:37:32 -0300

    use client

--- app/dashboard/teams/page.tsx
+++ app/dashboard/teams/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { Alert, AlertDescription, AlertTitle } from '@/app/components/ui/alert';
